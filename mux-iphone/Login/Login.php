12c4d1b48ba4f660793f0ef7db99d50040b53579
aasifazmi <mohamed.2016051@iit.ac.lk>
2019-12-14 00:10:14 +0530

    signup and login css

--- mux-iphone/Login/Login.php
+++ mux-iphone/Login/Login.php
@@ -4,7 +4,7 @@
 	?>
 	<?php include '../templates/header.php'; ?>
 	<header>
-	<link rel="stylesheet" href="../home-page/home.css">
+	<link rel="stylesheet" href="../Login/login.css">
 		<link rel="stylesheet" href="../assets/styles/header.css">
 		<script src="../assets/scripts/main.js"></script>
 	</header>
@@ -17,23 +17,12 @@ body {
   background-image: url('signupbk.jpg');
 }
 </style>
-    <!--<style>
-    button {background-color: #4CAF50;
-            border: none;
-            color: white;
-            padding: 15px 32px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 16px;}
-    </style>-->
-    
-    <?php include '../templates/header.php'; ?>
+ 
+ 
     <head>
         <meta name = "viewport" content = "width = device-width, initial-scale = 1">
-        <link rel = "stylesheet" href ="signup.css">
         <link rel = "stylesheet" href = "https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
-        <!--<link rel = "stylesheet" href = "sample.css">-->
+       
         
         <script src = "https://code.jquery.com/jquery-1.11.3.min.js"></script>
         <script src = "https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
@@ -41,22 +30,22 @@ body {
     <body>
     <div style="background-image: url('../assets/images/loginbk.jpg'); background-size: 100% 100%;">
         <br><br><br>
-        <div class="ui-grid-b">
-            <div class="ui-block-a">    
-            </div>
-            <div class="ui-block-b" >
+        <div class="ui-grid-solo">
+           <!-- <div class="ui-block-a">    
+            </div> -->
+            <div class="ui-block-a" align=center style="background-color: #ffffff">
                 <h1 align=center> Login </h1>
                 <br>
-                <form action="handler.php" method="post" >
+                <form align=left action="handler.php" method="post" >
                     <label for="email">Email</label>
                     <input type="text" name="email" id="email" value="" placeholder="" />
                     <label for="password">Password</label>
                     <input type="text" name="password" id="password" value="" placeholder="" />
                     </form>
-                <br>
+                
                 <!--<input class="button" type="button" name="signup" id="signup" value="Sign Up" width="50%"/>  -->
                 <button class="button">Login</button>           
-                <br><br>
+                
                 <hr>
                 <br>
                 <div align=center>
@@ -65,7 +54,7 @@ body {
                     <br>
                     <br>
                     <a href="#forgotpassword">Forgot your password?</a>
-                    <p>Dont have an account? <a href="#signup">Sign up</a> </p>  
+                    <p>Dont have an account? <a href="../signup/signup.php">Sign up</a> </p>  
                 </div>            
             </div>
         </div>
--- mux-iphone/Signup/signup.php
+++ mux-iphone/Signup/signup.php
@@ -1,41 +1,35 @@
+
+<?php 
+		session_start();
+	?>
+	<?php include '../templates/header.php'; ?>
+	<header>
+	<link rel="stylesheet" href="../signup/signup.css">
+		<link rel="stylesheet" href="../assets/styles/header.css">
+		<script src="../assets/scripts/main.js"></script>
+	</header>
+
+
 <!DOCTYPE html>
 <html>
-    <!--<style>
-    button {background-color: #4CAF50;
-            border: none;
-            color: white;
-            padding: 15px 32px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 16px;}
-    </style>-->
-<!--     <style>
-    body {
-        background-image: url('../assets/images/signupbk.jpg');
-        
-        }
-    </style> -->
-    <?php include '../templates/header.php'; ?>
     <head>
         <meta name = "viewport" content = "width = device-width, initial-scale = 1">
-        <link rel = "stylesheet" href ="signup.css">
         <link rel = "stylesheet" href = "https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
-        <!--<link rel = "stylesheet" href = "sample.css">-->
+       
         
         <script src = "https://code.jquery.com/jquery-1.11.3.min.js"></script>
         <script src = "https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
     </head> 
     <body>
-    <div style="background-image: url('../assets/images/signupbk.jpg'); background-size: 120% 120%;">
+    <div style="background-image: url('../assets/images/signupbk.jpg'); background-size: 100% 100%;">
         <br><br><br>
-        <div class="ui-grid-b">
-            <div class="ui-block-a">    
-            </div>
-            <div class="ui-block-b" style="background-color: #ffffff">
-                <h1 align=center> Sign Up</h1>
+        <div class="ui-grid-solo">
+           <!-- <div class="ui-block-a">    
+            </div> -->
+            <div class="ui-block-a" align=center style="background-color: #ffffff">
+                <h1 align=center> Sign Up </h1>
                 <br>
-                <form action="handler.php" method="post">
+                <form align=left action="handler.php" method="post">
                     <label for="name">Name</label>
                     <input type="text" name="name" id="name" value="" placeholder="" width="10px" />
                     <label for="number">Mobile Number</label>
@@ -47,10 +41,10 @@
                     <label for="conpassword">Confirm Password</label>
                     <input type="text" name="conpassword" id="conpassword" value="" placeholder="" />
                 </form>
-                <br>
-                <!--<input class="button" type="button" name="signup" id="signup" value="Sign Up" width="50%"/>  -->
+                
+                
                 <button class="button">Sign Up</button>           
-                <br><br>
+                
                 <hr>
                 <br>
                 <div align=center>
@@ -58,11 +52,11 @@
                     <img src="../assets/images/facebook.svg" alt="facebook" width="20%" hspace="5">
                     <br>
                     <br>
-                    <p>Already have an account? <a href="#login">Login</a> </p>  
-                </div>            
+                    <p>Already have an account? <a href="../Login/login.php">Login</a> </p>  
+                </div>          
             </div>
-        </div> 
-    </div>      
+        </div>
+    </div>           
     </body>
 </html>        
      
