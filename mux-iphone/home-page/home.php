c9300e1a1b8d6136856bb1c513c798834906568d
hansiSigirige2 <hsigirge@gmail.com>
2019-12-14 11:55:54 +0530

    slider

--- mux-iphone/home-page/home.php
+++ mux-iphone/home-page/home.php
@@ -3,12 +3,13 @@
 	?>
 	<?php include '../templates/header.php'; ?>
 
+<link rel="stylesheet" href="../assets/home-page/home.css">
 <!-- carousel indicators -->
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  <ol class="carousel-indicators" style ="top: 141px !important;">
+    <li data-target="#carouselExampleIndicators" style="border-radius: 50% !important; width: 10px !important;height: 10px !important;" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" style="border-radius: 50% !important; width: 10px !important;height: 10px !important;" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" style="border-radius: 50% !important; width: 10px !important;height: 10px !important;" data-slide-to="2"></li>
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
