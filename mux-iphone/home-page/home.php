5c7206f5aa076bd8480820d89fd36476128bdfcf
hansiSigirige2 <hsigirge@gmail.com>
2019-12-14 12:08:20 +0530

    slider 2

--- mux-iphone/home-page/home.php
+++ mux-iphone/home-page/home.php
@@ -4,7 +4,7 @@
 	<?php include '../templates/header.php'; ?>
 
 <link rel="stylesheet" href="../assets/home-page/home.css">
-<!-- carousel indicators -->
+<!-- carousel pics -->
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators" >
     <li data-target="#carouselExampleIndicators" style="border-radius: 50% !important; width: 10px !important;height: 10px !important;" data-slide-to="0" class="active"></li>
@@ -31,5 +31,34 @@
     <span class="sr-only">Next</span>
   </a>
 </div>
+
+<!-- carousel promo -->
+<div id="carouselpromo" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators" >
+    <li data-target="#carouselpromo" style="border-radius: 50% !important; width: 10px !important;height: 10px !important;" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselpromo" style="border-radius: 50% !important; width: 10px !important;height: 10px !important;" data-slide-to="1"></li>
+    <li data-target="#carouselpromo" style="border-radius: 50% !important; width: 10px !important;height: 10px !important;" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="../assets/images/img1.png" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../assets/images/img2.png" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../assets/images/img3.png" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselpromo" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselpromo" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
 
 <?php include '../templates/footer.php'; ?>
