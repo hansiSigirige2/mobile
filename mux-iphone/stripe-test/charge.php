4bb7010f73aec3ebc58778dcf868c4266c65a50f
dlakshman <dlakshman@virtusa.com>
2019-12-15 16:40:29 +0530

    stripe

---
+++ mux-iphone/stripe-test/charge.php
@@ -0,0 +1,16 @@
+<?php
+// Set your secret key: remember to change this to your live secret key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+require_once('../assets/plugins/stripe-php/stripe-php/init.php');
+\Stripe\Stripe::setApiKey('sk_test_OXCwF5UvnYeb5NmfSAHxCEfg00BAL9JCX8');
+
+// Token is created using Stripe Checkout or Elements!
+// Get the payment token ID submitted by the form:
+$token = $_POST['stripeToken'];
+$charge = \Stripe\Charge::create([
+    'amount' => 999,
+    'currency' => 'usd',
+    'description' => 'Example charge',
+    'source' => $token,
+]);
+?>
--- mux-iphone/stripe-test/stripe.php
+++ mux-iphone/stripe-test/stripe.php
@@ -1,9 +1,28 @@
-<?php 
-	session_start();
-	include '../templates/header.php';
-?>
+<html>
 
+<head> 
+  <script src="https://js.stripe.com/v3/"></script>
+</head>
+
+<body>
+  <form class="thin" action="charge.php" method="post" id="payment-form">
+    <div class="form-row">
+      <label for="card-element">
+        Credit or debit card
+      </label>
+      <div id="card-element">
+        <!-- A Stripe Element will be inserted here. -->
+      </div>
+  
+      <!-- Used to display Element errors. -->
+      <div id="card-errors" role="alert"></div>
+    </div>
+  
+    <button>Submit Payment</button>
+  </form>
+</body>
 
 
 <script src="../assets/scripts/stripe.js"></script>
-<?php include '../templates/footer.php'; ?>
+
+</html>
