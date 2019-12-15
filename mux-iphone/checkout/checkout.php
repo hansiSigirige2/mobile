201a3c9d9f710171653910997c35b70decaf9ef1
dlakshman <dlakshman@virtusa.com>
2019-12-15 11:15:56 +0530

    footer pad

--- mux-iphone/checkout/checkout.php
+++ mux-iphone/checkout/checkout.php
@@ -1,29 +1,28 @@
-
-
 <?php 
 	session_start();
 	include '../templates/header.php';
 ?>
-	
+
 
 <header>
-<link rel="stylesheet" href="checkout.css">
+	<link rel="stylesheet" href="checkout.css">
 </header>
+
 <!-- checkout -->
 <div class="ui-grid-b checkout-container">
-    <div class="ui-block-a checkout-title">CHECKOUT</div>
-    <!-- cart -->
-    <div class="ui-grid-b card-container">
-    <div class="ui-block-a cart-title">CART</div>
-    <div class="ui-block-c">
-        <img class="edit-image" src="../assets/images/edit.png" alt="">
-    </div>
+	<div class="ui-block-a checkout-title">CHECKOUT</div>
+	<!-- cart -->
+	<div class="ui-grid-b card-container">
+		<div class="ui-block-a cart-title">CART</div>
+		<div class="ui-block-c">
+			<img class="edit-image" src="../assets/images/edit.png" alt="">
+		</div>
 
-    <!-- cards -->
-    <div class="ui-block-a cards-small-container">
-<?php 	
-		$cart_json = file_get_contents('../data/cart.json');
-		$cart_arr = json_decode($cart_json, true);
+		<!-- cards -->
+		<div class="ui-block-a cards-small-container">
+			<?php 	
+			$cart_json = file_get_contents('../data/cart.json');
+			$cart_arr = json_decode($cart_json, true);
         
 			if(count($cart_arr) === 0 ){
 				echo '<div class ="ui-grid-a">Sorry no items found</div>';
@@ -49,31 +48,23 @@
 						echo '</div>';
 				}
 				echo '</div>';
-			}
-			
-		
-		
-		
-?>
-<!-- cards cart end -->
-   
+			}?>
+			<!-- cards cart end -->
+		</div>
+		<!-- cards -->
+	</div>
+	<!-- cart -->
 </div>
-    <!-- cards -->
 
-    </div>
-      <!-- cart -->
+
 <!-- delivery address -->
-<div class="delivery-address-container">
-    <div class="deliver-title">Delivery ADDRESS</div>
-    <div class="address-box">
-    <textarea class ="address-input"></textarea>
-    </div>
+<div class="ui-grid-solo delivery-address-container">
+	<div class="deliver-title">Delivery ADDRESS</div>
+		<div class="address-box">
+		<textarea class="address-input"></textarea>
+	</div>
 </div>
 
 <div class="footer-padding"></div>
-   
 
-
-   
-
-<?php include '../templates/footer.php'; ?>
+	<?php include '../templates/footer.php'; ?>
