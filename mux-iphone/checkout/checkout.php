cccf9e26f4a246beec3f94d0200b1627fc655ff4
hansiSigirige2 <hsigirge@gmail.com>
2019-12-15 09:40:35 +0530

    checkout cards

--- mux-iphone/checkout/checkout.php
+++ mux-iphone/checkout/checkout.php
@@ -13,15 +13,57 @@
 <div class="ui-grid-b checkout-container">
     <div class="ui-block-a checkout-title">CHECKOUT</div>
     <!-- cart -->
-    <div class="ui-block-a card-container">
+    <div class="ui-grid-b card-container">
     <div class="ui-block-a cart-title">CART</div>
-    
-        <img class="ui-block-c edit-image" src="../assets/images/edit.png" alt="">
+    <div class="ui-block-c">
+        <img class="edit-image" src="../assets/images/edit.png" alt="">
+    </div>
+
+    <!-- cards -->
+    <div class="ui-block-a cards-small-container">
+<?php 	
+		$cart_json = file_get_contents('../data/cart.json');
+		$cart_arr = json_decode($cart_json, true);
+        
+			if(count($cart_arr) === 0 ){
+				echo '<div class ="ui-grid-a">Sorry no items found</div>';
+
+			}else{
+				if ($_SESSION['orientation'] == "land") {
+					echo '<div class ="ui-grid-b">';
+				} 
+				else {
+					echo '<div class ="ui-grid-a">';						
+				}		
+				
+				for ($x = 0; $x <count($cart_arr); $x++) {
+					if ($x === 0){
+						echo '<div class ="ui-block-a product-padding">';
+					}
+					else{
+						echo '<div class ="ui-block-b product-padding">';
+					}
+						echo'<div class="card cart-card">';
+						echo '<img class="card-img-top card-image-border" src="'.$cart_arr[$x]['image'].'" alt="Card image cap">';					
+						echo '</div>';
+						echo '</div>';
+				}
+				echo '</div>';
+			}
+			
+		
+		
+		
+?>
+<!-- cards cart end -->
    
+</div>
+    <!-- cards -->
+
     </div>
       <!-- cart -->
-</div>
-<!-- checkout -->
+   
+   
 
 
    
